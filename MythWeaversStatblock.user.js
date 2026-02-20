a0d53ba4b6bba7a175e9323386989e4a7681770c
BlackPhoenix <demerss@gmail.com>
2026-02-20 14:31:56 -0500

    Exits if private notes field does not exist (#8)
    * Add check for private notes field in StartProcess
    
    Check for the existence of the private notes field before proceeding with the process.
    
    * Update version from 4.5 to 4.6 and change return value
    
    * Prevent waitForKeyElements continuation
    
    Added return statement to prevent waitForKeyElements from continuing.

--- MythWeaversStatblock.user.js
+++ MythWeaversStatblock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Myth-Weavers Autofill
 // @namespace    http://tampermonkey.net/
-// @version      4.5
+// @version      4.6
 // @description  A better statblock generator
 // @author       BlackPhoenix
 // @match        https://www.myth-weavers.com/sheets/?id=*
@@ -43,6 +43,12 @@ waitForKeyElements(
     );
 
 function StartProcess() {
+    const privateNotes = document.getElementsByName(privateNotesField)[0];
+    if (!privateNotes) {
+        console.log("Private Notes field not found yet, terminating.");
+        return false;
+    }
+    
     // Add a new button "Statblock" to the left of the Save button.
     var sbButton = document.createElement("BUTTON");
     sbButton.innerHTML = "Autofill";
@@ -50,6 +56,7 @@ function StartProcess() {
     sbButton.onclick = WriteStatblock;
     var sheetControls = document.getElementsByClassName("nav-item")[1];
     sheetControls.insertBefore(sbButton, sheetControls.childNodes[0]);
+    return false;    // Stop waitForKeyElements()
 }
 
 function WriteStatblock() {
